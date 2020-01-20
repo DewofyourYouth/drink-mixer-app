6cd66184cfffcce2f302a740466e924f1a8885c6
Jacob Shore <jacobshore@gmail.com>
2020-01-20 19:42:11 +0200

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,32 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 
 function style() {
-    // 1. where is my scss file?
-    return gulp.src('./scss/**/*.scss')
-    // 2. pass that file through sass compiler
-    .pipe(sass())
-    // 3. where to save compiled css?
-    .pipe(gulp.dest('./css'))
-    // 4. stream changes to all browsers
-    .pipe(browserSync.stream());
+  // 1. where is my scss file?
+  return (
+    gulp
+      .src('./scss/**/*.scss')
+      // 2. pass that file through sass compiler
+      .pipe(sass())
+      .pipe(cleanCSS({ debug: true }))
+      // 3. where to save compiled css?
+      .pipe(gulp.dest('./css'))
+      // 4. stream changes to all browsers
+      .pipe(browserSync.stream())
+  );
 }
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload)
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
--- js/main.js
+++ js/main.js
@@ -1,6 +1,75 @@
 const App = new Vue({
   el: '#app',
   data: {
-    msg: 'Hello Vue!'
+    drinkName: '',
+    alcohols: [
+      'rum 🥃',
+      'scotch 🥃',
+      'tequilla 🥃',
+      'bourbon 🥃',
+      'beer 🍺',
+      'gin 🍸',
+      'whiskey 🥃',
+      'vodka 🍸',
+      'red wine 🍷',
+      'white wine 🥂',
+      'red vermouth 🍷',
+      'white vermouth 🥂'
+    ],
+    juices: [
+      'apple juice 🍎',
+      'orange juice 🍊',
+      'lemon juice 🍋',
+      'lime juice 🍋',
+      'tomato juice 🍅',
+      'cherry juice 🍒',
+      'pineapple juice 🍍',
+      'cola 🥤',
+      'seltzer ✨'
+    ],
+    garnishes: [
+      'lemon slice 🍋',
+      'lime slice 🍋',
+      'orange slice 🍊',
+      'pineapple slice 🍍',
+      'cocktail olive 🍸',
+      'cocktail onion',
+      'orange zest 🍊',
+      'lemon zest 🍋',
+      'salt 🧂',
+      'whipped cream 🍦'
+    ],
+    actions: ['serve with ice ❄️', 'shake with ice ❄️', 'stir 🥄'],
+    ingredients: {
+      alcohol: [],
+      juices: [],
+      garnishes: []
+    }
+  },
+  methods: {
+    addToIngredients: function(event) {
+      const ingredient = event.target.innerText;
+      let arr = '';
+      const myIngredients = this.ingredients;
+      console.log(event.target.className);
+      switch (event.target.className) {
+        case 'alc-btn':
+          arr = myIngredients.alcohol;
+          break;
+        case 'juice-btn':
+          arr = myIngredients.juices;
+          break;
+        case 'garnish-btn':
+          arr = myIngredients.garnishes;
+          break;
+        default:
+          return;
+      }
+      if (arr.filter(i => i.name === ingredient).length === 0) {
+        arr.push({ name: ingredient });
+      } else {
+        console.log(`${ingredient} already exists`);
+      }
+    }
   }
 });
